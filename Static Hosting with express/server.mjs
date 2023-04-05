b20a4cee7ded30c625d8bb9ba1edbe4703fe6a70
Ahmer Siddiqui <siddiquiahmer970@gmail.com>
2023-04-05 16:58:49 +0500

    Setting static server file

--- Static Hosting with express/server.mjs
+++ Static Hosting with express/server.mjs
@@ -1,27 +1,31 @@
+import express from 'express';
 import path from 'path';
-import express from 'express'
-// const express = require('express') // old so reuired not allow
+
 const app = express()
 const port = process.env.PORT || 5001;
 
 app.get('/abc', (req, res) => {
-  console.log("request ip:" , req.ip);
-  res.send('Hello World! ' + new Date().toString());
+    console.log("request ip: ", req.ip);
+    res.send('Hello World! ' + new Date().toString());
 })
-app.get('/getweather', (req, res) => {
-  console.log("request ip:" , req.ip);
-  res.send('Hello World! ' + new Date().toString());
+app.get('/weather', (req, res) => {
+    console.log("request ip: ", req.ip);
+    res.send({
+        temp: 30,
+        humidity: 72,
+        serverTime: new Date().toString()
+    });
 })
-app.get('/gettime', (req, res) => {
-  console.log("request ip:" , req.ip);
-  res.send('Hello World! ' + new Date().toString());
+app.get('/time', (req, res) => {
+    console.log("request ip: ", req.ip);
+    res.send('Hello World! ' + new Date().toString());
 })
 
 const __dirname = path.resolve();
-app.use('/', express.static(path.join(__dirname ,'./web/build')))
-app.use('*', express.static(path.join(__dirname ,'./web/build')))
+app.use('/', express.static(path.join(__dirname, './web/build')))
+app.use('*', express.static(path.join(__dirname, './web/build')))
 
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-}  )
+    console.log(`Example app listening on port ${port}`)
+})
